9634f8146e52f53b2866ac93e893d2368e1cf80b
GhostUser <35362972+GhostUser@users.noreply.github.com>
2020-10-22 20:26:14 +0530

    Added the main game loop

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import pygame 
+import pygame
+from pygame import event 
+import sys
+
+
 
 class Screen:
-    NUM_BLOCKS_X = 40
-    NUM_BLOCKS_Y = 30
-    block_size = pygame.DEFAULT_RECT_SIZE
+    NUM_BLOCKS_X = 16
+    NUM_BLOCKS_Y = 20
+    block_size = 30
     WIDTH, HEIGHT = block_size * NUM_BLOCKS_X, block_size * NUM_BLOCKS_Y
     FPS = 32
 
+
+
+def Game_loop():
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+            sys.exit()
+
+        keys=pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE]:
+            pass
 
 
 def main():
@@ -14,10 +30,14 @@ def main():
     pygame.font.init()
     pygame.display.set_caption("Flappy Bird")
     pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
+    pygame.time.delay(50)
+    run=True
+    while run:
+        Game_loop()
 
+    pygame.quit()
 
-
-if __name__=='main':
+if __name__=='__main__':
     main()
 
 
